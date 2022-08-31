472c5efebb6b4dba0ea9e814841d38bf585ecef6
Zulfiq <zulfiqar.siddique@gmail.com>
2022-08-30 23:26:10 -0500

    ZZZZZZ

---
+++ src/test/java/StepDefinition/Cart1.java
@@ -0,0 +1,35 @@
+package StepDefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Cart1 {
+
+	@Given("^Launch appplication(\\d+) url$")
+	public void launch_appplication_url(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("^Click Samsung$")
+	public void click_Samsung() throws Throwable {
+	 
+	}
+
+	@When("^Click AddToCart(\\d+)$")
+	public void click_AddToCart(int arg1) throws Throwable {
+	 
+	}
+
+	@Then("^Click Cart(\\d+)$")
+	public void click_Cart(int arg1) throws Throwable {
+	 
+	}
+
+	@Then("^Verify item added in Cart(\\d+)$")
+	public void verify_item_added_in_Cart(int arg1) throws Throwable {
+	 
+	}
+	
+}
---
+++ src/test/resources/Features/Cart1.feature
@@ -0,0 +1,9 @@
+Feature: This is a Cart Functionality
+
+
+Scenario: Verify user added item in cart1
+Given Launch appplication1 url
+Then Click Samsung
+When Click AddToCart1
+Then Click Cart1
+Then Verify item added in Cart1
